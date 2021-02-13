7693f97d22a6683a23c721b6fee5264b669f19e9
JoaoPedroCAS <joaosa@alunos.utfpr.edu.br>
2021-02-13 12:14:28 -0300

    Project Created

---
+++ backend/src/main/java/com/movingdelivery/movingdelivery/MovingdeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.movingdelivery.movingdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovingdeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovingdeliveryApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/movingdelivery/movingdelivery/MovingdeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movingdelivery.movingdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovingdeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
