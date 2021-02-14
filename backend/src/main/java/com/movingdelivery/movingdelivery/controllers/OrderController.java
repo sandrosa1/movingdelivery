c6693bdcb815146a471b8de244d5f9e4848db332
sandrosa1 <sandro.sa@fatec.sp.gov.br>
2021-02-13 21:45:47 -0300

    post/ordes

--- backend/src/main/java/com/movingdelivery/movingdelivery/controllers/OrderController.java
+++ backend/src/main/java/com/movingdelivery/movingdelivery/controllers/OrderController.java
@@ -1,12 +1,16 @@
 package com.movingdelivery.movingdelivery.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.movingdelivery.movingdelivery.dto.OrderDTO;
 import com.movingdelivery.movingdelivery.services.OrderService;
@@ -27,5 +31,12 @@ public class OrderController {
 		List<OrderDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
-
+	//Criando end point para salvar
+	//Retornar code 201
+	@PostMapping
+	public ResponseEntity<OrderDTO> insert(@RequestBody OrderDTO dto){
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+	}
 }
--- backend/src/main/java/com/movingdelivery/movingdelivery/services/OrderService.java
+++ backend/src/main/java/com/movingdelivery/movingdelivery/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.movingdelivery.movingdelivery.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,8 +9,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.movingdelivery.movingdelivery.dto.OrderDTO;
+import com.movingdelivery.movingdelivery.dto.ProductDTO;
 import com.movingdelivery.movingdelivery.entities.Order;
+import com.movingdelivery.movingdelivery.entities.OrderStatus;
+import com.movingdelivery.movingdelivery.entities.Product;
 import com.movingdelivery.movingdelivery.repositories.OrderRepository;
+import com.movingdelivery.movingdelivery.repositories.ProductRepository;
 
 
 //Primeira ingeção de dependencia de componentes
@@ -19,10 +24,27 @@ public class OrderService {
 	@Autowired
 	private OrderRepository repository;
 	
+	@Autowired
+	private ProductRepository productRepository;
+	
 	@Transactional(readOnly = true)
 	public List<OrderDTO> findAll(){
 		List<Order> list = repository.findOrdesSwithProducts();
 		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}
+	
+	
+	//Metodo para inserir um novo pedido
+	@Transactional
+	public OrderDTO insert(OrderDTO dto){
+		Order order = new Order(null, dto.getName(), dto.getAddress(), Instant.now(), OrderStatus.PENDING);
+		
+		for (ProductDTO p : dto.getProducts()) {
+			Product product = productRepository.getOne(p.getId());
+			order.getProducts().add(product);
+		}
+		order = repository.save(order);
+		return new OrderDTO(order);
 	}
 	
 
